af6e6fbe268903e22271d00c77e51457b2e1ad7f
bigcatplichta <bigcatplichta@gmail.com>
2020-11-02 20:21:43 -0700

    add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d3c6d',
+        secondary: '#136871',
+        tertiary: '#890200',
+        thblack: '#090a0f',
+        thgrey: '#dfdfd8'
+      }
+    }
+  },
   purge: ["./src/**/*.js"],
-  theme: {},
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
 };
+
