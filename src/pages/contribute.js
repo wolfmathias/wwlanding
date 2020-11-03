af65b017a5f2fed0917fc7e1c4330355eddfc94f
bigcatplichta <bigcatplichta@gmail.com>
2020-11-02 22:07:56 -0700

    add file

---
+++ src/pages/contribute.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+function ContributePage() {
+    return (
+        <Layout>
+            <SEO
+                keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+                title="Contact"
+            />
+
+        </Layout>
+    )
+}
+
+export default ContributePage;
