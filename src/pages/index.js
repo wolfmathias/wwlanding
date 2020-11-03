28e2f350e34a4ae9dcfa34867308815877bee453
bigcatplichta <bigcatplichta@gmail.com>
2020-11-02 18:32:51 -0700

    remove section and update bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import tigerCardboard from "../images/tigercardboard.png"
 
 function IndexPage() {
   return (
@@ -11,31 +11,13 @@ function IndexPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Home"
       />
-
-      <section className="text-center">
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
-
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
-        </p>
-      </section>
+      <div className="h-screen bg-local bg-cover bg-center" style={{ backgroundImage:`url(${tigerCardboard})`}}>
+        <div className="h-full pt-20">
+          <div className="uppercase text-4xl text-gray-300 font-medium tracking-widest p-6">
+            
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
