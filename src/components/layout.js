6614c6dab0efd7a8c8ef1f083268d667c6d945d7
bigcatplichta <bigcatplichta@gmail.com>
2020-11-03 12:06:55 -0700

    add site/console comments

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,9 +15,11 @@ function Layout({ children }) {
     }
   `);
 
+  console.log("Oh hey! Inspecting my elements, huh? Why not help me build this app? (I'm also looking for work) If you're reading this it means I haven't gotten to the frontend yet. Check out my github (github.com/mattplichtawild) or email me at mattplichtawild@gmail.com to help me help some animals :)")
+
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
-    <div dangerouslySetInnerHTML={{ __html: "<!-- Hi! I'm glad you're here and reading this, it means you can help! I'm building this application on my own in my free time, I learned programming just to make this idea happen (and find a job). Check out my github at https://www.github.com/mattplichtawild and email me if you want to contribute to the project! I'm also open to work and seeking roles as a software engineer -->" }} />
+    <div dangerouslySetInnerHTML={{ __html: "<!-- Hi! I'm glad you're here and reading this, it means you can help! I'm building this application on my own in my free time, I learned programming just to make this idea happen (and find a job). I threw this landing page together in a few hours using Gatsby so I could focus on building the app with Django and getting hosted to AWS. I'll need help with the frontend, as well! Check out my github at https://www.github.com/mattplichtawild and email me if you want to contribute to the project! I'm also open to work and seeking roles as a software engineer -->" }} />
       <Header />
 
       <main className="flex-1 w-full mx-auto">
