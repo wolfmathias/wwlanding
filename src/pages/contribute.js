d7181034871964a773ae8e5cd07ed10848d49dde
bigcatplichta <bigcatplichta@gmail.com>
2020-11-04 13:56:20 -0700

    add icons

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -1,16 +1,33 @@
 import React from "react";
-
+import { GitHub, Mail } from 'react-feather'
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 function ContributePage() {
+    let github = 'github.com/mattplichtawild/django-wildwish'
+    let email = 'mattplichtawild@gmail.com'
+
     return (
         <Layout>
             <SEO
                 keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-                title="Contact"
+                title="Contribute"
             />
-
+            <section className="container mt-24">
+                <div className="grid grid-rows-3">
+                    
+                    {github && (
+                    <a  href={github}>
+                        <GitHub /> {github}
+                    </a>
+                    )}
+                    {email && (
+                    <a className="grid-span-1" href={`mailto:${email}`}>
+                        <Mail /> <span>{email}</span>
+                    </a>
+                    )}
+                </div>
+            </section>
         </Layout>
     )
 }
