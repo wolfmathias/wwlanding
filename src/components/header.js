66afeff4e95b8928d54f6c95799dbb1ca2a0a671
bigcatplichta <bigcatplichta@gmail.com>
2020-11-04 14:25:57 -0700

    remove header link

--- src/components/header.js
+++ src/components/header.js
@@ -45,10 +45,10 @@ function Header() {
               route: `/signup`,
               title: `Sign Up`,
             },
-            {
-              route: `/contribute`,
-              title: `Contribute`
-            }
+            // {
+            //   route: `/contribute`,
+            //   title: `Contribute`
+            // }
           ].map((link) => (
             <Link
               className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
