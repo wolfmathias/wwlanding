13e0b1318fd906b3a1c7bb98c4f9668c5cf5efd1
bigcatplichta <bigcatplichta@gmail.com>
2020-11-04 16:08:42 -0700

    update image imports

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,20 +2,20 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
+// import abductionIllustration from "../images/abduction-illustration.svg";
 
 function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not found" />
       <div>
-        <img
+        {/* <img
           alt="Ghost getting abducted by aliens"
           className="block mx-auto w-1/2"
           src={abductionIllustration}
-        />
+        /> */}
         <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
+          This isn&apos;t the page you&apos;re looking for...
         </h2>
       </div>
     </Layout>
