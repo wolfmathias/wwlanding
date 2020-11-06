453c4930721a8ae98abd90506ea9be3a93278a4f
bigcatplichta <bigcatplichta@gmail.com>
2020-11-05 20:14:46 -0700

    add link to /

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 // import abductionIllustration from "../images/abduction-illustration.svg";
@@ -6,18 +7,27 @@ import Layout from "../components/layout";
 function ThankYouPage() {
   return (
     <Layout>
-      <div>
+      <div className="mt-24 text-center leading-loose tracking-wide">
         {/* <img
           alt="Ghost getting abducted by aliens"
           className="block mx-auto w-1/2"
           src={abductionIllustration}
         /> */}
-        <h2 className="text-2xl font-bold inline-block my-8 p-3">
+        <h2 className="text-4xl font-bold inline-block my-8 p-3">
             Thank you!
         </h2>
+        <br/>
         <p className="inline-block my-8 p-3">
-            We&apos;ll reach out to sign your animals up soon!
+            You&apos;ll get an email when we launch.
         </p>
+      </div>
+      <div className="text-center">
+        <Link
+            className="underline text-sm"
+            href="/"
+        >
+            Go Back
+        </Link> 
       </div>
     </Layout>
   );
