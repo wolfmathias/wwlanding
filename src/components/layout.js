abb45be50ca8bdf4e96824cb04ebcc666aecc3e3
bigcatplichta <bigcatplichta@gmail.com>
2021-04-12 15:34:05 -0600

    change dev greeting

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,11 +15,11 @@ function Layout({ children }) {
   //   }
   // `);
 
-  console.log("Oh hey! Inspecting my elements, huh? Why not help me build this app? (I'm also looking for work) If you're reading this it means I haven't gotten to the frontend yet. Check out my github (github.com/mattplichtawild) or email me at mattplichtawild@gmail.com to help me help some animals :)")
+  console.log("Oh hey! Inspecting my elements, huh? Why not help me build this app? ( dev.wildwish.org ) I'm also looking for work! Check out my github (github.com/mattplichtawild) or email me at mattplichtawild@gmail.com to help me help some animals :)")
 
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
-    <div dangerouslySetInnerHTML={{ __html: "<!-- Oh hey! Inspecting my elements, huh? Why not help me build this app? (I'm also looking for work) If you're reading this it means I haven't gotten to the frontend yet. Check out my github (github.com/mattplichtawild) or email me at mattplichtawild@gmail.com to help me help some animals :) -->" }} />
+    <div dangerouslySetInnerHTML={{ __html: "<!-- Oh hey! Inspecting my elements, huh? Why not help me build this app? ( dev.wildwish.org ) I'm also looking for work! Check out my github (github.com/mattplichtawild) or email me at mattplichtawild@gmail.com to help me help some animals :) -->" }} />
       <Header />
 
       <main className="flex-1 w-full mx-auto">
